1c86ca73cb4b042913397fa69fd3a90982161d50
Vasileios Souvatzis <souvatzisbill@gmail.com>
2018-08-21 17:59:28 +0300

    removed unused db.py

--- tutorials/flask_restful/backend/db.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-
-client = pymongo.MongoClient('localhost', 27017)
-db = client['yelp']
-
-
-def find(collection_name, limit):
-    return db[collection_name].find({}, {'_id': False}).limit(limit)
